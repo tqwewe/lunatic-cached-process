d56789a5066e97a7d0f2cb23067a69c07b17f459
Ari Seyhun <ariseyhun@live.com.au>
2023-03-03 17:05:15 +1030

    feat: add serializer generic

--- src/lib.rs
+++ src/lib.rs
@@ -25,10 +25,10 @@
 
 use std::cell::RefCell;
 
-use lunatic::{process::ProcessRef, Process, ProcessLocal};
+use lunatic::{process::ProcessRef, serializer::Bincode, Process, ProcessLocal};
 use serde::{Deserialize, Serialize};
 
-pub type ProcessCached<T> = CachedProcess<Process<T>>;
+pub type ProcessCached<T, S = Bincode> = CachedProcess<Process<T, S>>;
 pub type ProcessRefCached<T> = CachedProcess<ProcessRef<T>>;
 
 /// Cached process to avoid looking up a global process multiple times.
@@ -47,52 +47,89 @@ impl<T> CachedProcess<T> {
             process_name: name,
         }
     }
+}
 
-    /// Sets the process cache value.
-    pub fn set(&self, value: T) {
+/// Trait for accessing a static process local cache.
+pub trait CachedLookup<'a, T> {
+    /// Looks up a process by its name, and caches the result.
+    /// Subsequent calls will used the cached value.
+    fn get(&'a self) -> Option<T>;
+
+    /// Sets the cached lookup. This will prevent any lookups from being made,
+    /// since subsequent calls to [`CachedLookup::get`] will return this cached value.
+    fn set(&'a self, value: T);
+
+    /// Resets the cache, causing the next call to [`CachedLookup::get`] to lookup the process again.
+    fn reset(&'a self);
+}
+
+impl<T, S> CachedLookup<'static, Process<T, S>> for ProcessLocal<ProcessCached<T, S>> {
+    #[inline]
+    fn get(&'static self) -> Option<Process<T, S>> {
+        self.with(|proc| lookup(proc, |name| Process::lookup(name)))
+    }
+
+    #[inline]
+    fn set(&'static self, value: Process<T, S>) {
+        self.with(|proc| CachedLookup::set(proc, value))
+    }
+
+    #[inline]
+    fn reset(&'static self) {
+        self.with(CachedLookup::reset)
+    }
+}
+
+impl<T, S> CachedLookup<'static, Process<T, S>> for ProcessCached<T, S> {
+    #[inline]
+    fn get(&'static self) -> Option<Process<T, S>> {
+        lookup(self, |name| Process::lookup(name))
+    }
+
+    #[inline]
+    fn set(&'static self, value: Process<T, S>) {
         *self.lookup_state.borrow_mut() = LookupState::Present(value);
     }
 
-    /// Resets the process cache, causing subsequent calls to lookup the process again.
-    pub fn reset(&self) {
+    #[inline]
+    fn reset(&'static self) {
         *self.lookup_state.borrow_mut() = LookupState::NotLookedUp;
     }
 }
 
-/// Trait for accessing a static process local cache.
-pub trait CachedLookup<T> {
-    /// Looks up a process by its name, and caches the result.
-    /// Subsequent calls will used the cached value.
-    fn get(self) -> Option<T>;
+impl<T> CachedLookup<'static, ProcessRef<T>> for ProcessLocal<ProcessRefCached<T>> {
+    #[inline]
+    fn get(&'static self) -> Option<ProcessRef<T>> {
+        self.with(|proc| lookup(proc, |name| ProcessRef::lookup(name)))
+    }
 
-    /// Sets the cached lookup. This will prevent any lookups from being made,
-    /// since subsequent calls to [`CachedLookup::get`] will return this cached value.
-    fn set(self, value: T);
+    #[inline]
+    fn set(&'static self, value: ProcessRef<T>) {
+        self.with(|proc| CachedLookup::set(proc, value))
+    }
 
-    /// Resets the cache, causing the next call to [`CachedLookup::get`] to lookup the process again.
-    fn reset(self);
+    #[inline]
+    fn reset(&'static self) {
+        self.with(CachedLookup::reset)
+    }
 }
 
-macro_rules! impl_cached_lookup {
-    ($ty:ident, $cache_ty:ident) => {
-        impl<T> CachedLookup<$ty<T>> for &'static ProcessLocal<$cache_ty<T>> {
-            fn get(self) -> Option<$ty<T>> {
-                lookup(self, |name| $ty::lookup(name))
-            }
+impl<T> CachedLookup<'static, ProcessRef<T>> for ProcessRefCached<T> {
+    #[inline]
+    fn get(&'static self) -> Option<ProcessRef<T>> {
+        lookup(self, |name| ProcessRef::lookup(name))
+    }
 
-            fn set(self, value: $ty<T>) {
-                set(self, value);
-            }
+    #[inline]
+    fn set(&'static self, value: ProcessRef<T>) {
+        *self.lookup_state.borrow_mut() = LookupState::Present(value);
+    }
 
-            fn reset(self) {
-                reset(self)
-            }
-        }
-    };
+    #[inline]
+    fn reset(&'static self) {
+        *self.lookup_state.borrow_mut() = LookupState::NotLookedUp;
+    }
 }
-
-impl_cached_lookup!(Process, ProcessCached);
-impl_cached_lookup!(ProcessRef, ProcessRefCached);
 
 /// Macro for defining a process local lookup cache for processes.
 ///
@@ -150,39 +187,30 @@ impl<T> Default for LookupState<T> {
     }
 }
 
-fn lookup<F, T>(cached_process: &'static ProcessLocal<CachedProcess<T>>, f: F) -> Option<T>
+#[inline]
+fn lookup<F, T>(proc: &CachedProcess<T>, f: F) -> Option<T>
 where
     F: Fn(&'static str) -> Option<T>,
     T: Clone,
 {
-    cached_process.with(|proc| {
-        let proc_ref = proc.lookup_state.borrow();
-        match &*proc_ref {
-            LookupState::NotLookedUp => {
-                std::mem::drop(proc_ref);
-                match f(proc.process_name) {
-                    Some(process) => {
-                        *proc.lookup_state.borrow_mut() = LookupState::Present(process.clone()); // TODO: Replace clone with copy
-                        Some(process)
-                    }
-                    None => {
-                        *proc.lookup_state.borrow_mut() = LookupState::NotPresent;
-                        None
-                    }
+    let proc_ref = proc.lookup_state.borrow();
+    match &*proc_ref {
+        LookupState::NotLookedUp => {
+            std::mem::drop(proc_ref);
+            match f(proc.process_name) {
+                Some(process) => {
+                    *proc.lookup_state.borrow_mut() = LookupState::Present(process.clone()); // TODO: Replace clone with copy
+                    Some(process)
+                }
+                None => {
+                    *proc.lookup_state.borrow_mut() = LookupState::NotPresent;
+                    None
                 }
             }
-            LookupState::NotPresent => None,
-            LookupState::Present(process) => {
-                Some(process.clone()) // TODO: Replace clone with copy
-            }
         }
-    })
-}
-
-fn set<T>(cached_process: &'static ProcessLocal<CachedProcess<T>>, value: T) {
-    cached_process.with(|proc| proc.set(value))
-}
-
-fn reset<T>(cached_process: &'static ProcessLocal<CachedProcess<T>>) {
-    cached_process.with(|proc| proc.reset())
+        LookupState::NotPresent => None,
+        LookupState::Present(process) => {
+            Some(process.clone()) // TODO: Replace clone with copy
+        }
+    }
 }
